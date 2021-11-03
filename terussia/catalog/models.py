5ee8db6d39db91f04e0300ca5b011437a3075748
0RedSnake0 <sulwirld@gmail.com>
2021-11-03 21:52:34 +0300

    Create apps: accounts, catalog

---
+++ terussia/catalog/models.py
@@ -0,0 +1,2 @@
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
---
+++ terussia/catalog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        template_name="catalog/index.html",
+        context={}
+    )
