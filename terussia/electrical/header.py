051151f74794603eece850ef718740162ff030de
0RedSnake0 <sulwirld@gmail.com>
2021-11-16 22:08:56 +0300

    Update header styles.

--- terussia/electrical/header.py
+++ terussia/electrical/header.py
@@ -41,35 +41,19 @@ DROPDOWN_1 = {
         "content": {
             "Кабельная_арматура_РАЙХЕМ_1-35_кВ":{
                 "link":"https://terussia.ru/raychem-mv-cas/",
-                "content":{
-                    "Арматура_СИП_SIMEL_каталог":"https://bit.ly/2x1m5Jb",
-                    "Типовой_проект_СИП4":"https://bit.ly/3oh1q9O",
-                    "Типовой_проект_СИП3":"https://bit.ly/2HeLVi4",
-                },
+                "content":{},
             },
             "Кабельная_арматура_РАЙХЕМ_52-123_кВ":{
                 "link":"https://bit.ly/34YLorL/",
-                "content":{
-                    "Арматура_СИП_SIMEL_каталог":"https://bit.ly/2x1m5Jb",
-                    "Типовой_проект_СИП4":"https://bit.ly/3oh1q9O",
-                    "Типовой_проект_СИП3":"https://bit.ly/2HeLVi4",
-                },
+                "content":{},
             },
             "Кабельная_арматура_РАЙХЕМ_110-220_кВ":{
                 "link":"https://bit.ly/34YLorL",
-                "content":{
-                    "Арматура_СИП_SIMEL_каталог":"https://bit.ly/2x1m5Jb",
-                    "Типовой_проект_СИП4":"https://bit.ly/3oh1q9O",
-                    "Типовой_проект_СИП3":"https://bit.ly/2HeLVi4",
-                },
+                "content":{},
             },
             "Кабельная_арматура_ЭНЕРГО_6-10_кВ":{
                 "link":"https://bit.ly/2yGXqKb",
-                "content":{
-                    "Арматура_СИП_SIMEL_каталог":"https://bit.ly/2x1m5Jb",
-                    "Типовой_проект_СИП4":"https://bit.ly/3oh1q9O",
-                    "Типовой_проект_СИП3":"https://bit.ly/2HeLVi4",
-                },
+                "content":{},
             },
             "Арматура_СИП_SIMEL":{
                 "link":"https://terussia.ru/catalogs/%D0%9A%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%20%D0%B0%D1%80%D0%BC%D0%B0%D1%82%D1%83%D1%80%D0%B0%20%D0%A1%D0%98%D0%9F%20%D0%A1%D0%98%D0%9C%D0%95%D0%9B%D0%AC%20-%202017.pdf",
@@ -98,28 +82,58 @@ DROPDOWN_1 = {
         } 
     },
     
-    # "ПОДДЕРЖКА": {
-    #     "title":"ПОДДЕРЖКА",
-    #     "link":"#", 
-    #     "content": {
-    #         "Каталоги_продукции":"https://terussia.ru/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B8-%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%86%D0%B8%D0%B8",
-    #         "Техподдержка_и_обучение_110_и_220_кВ":"#",
-    #         "Обучение":"https://terussia.ru/%d0%be%d0%b1%d1%83%d1%87%d0%b5%d0%bd%d0%b8%d0%b5/",
-    #         "Техподдержка_и_обучение_до_35_кВ":"https://terussia.ru/%d1%82%d0%b5%d1%85%d0%bf%d0%be%d0%b4%d0%b4%d0%b5%d1%80%d0%b6%d0%ba%d0%b0-%d0%b8-%d0%be%d0%b1%d1%83%d1%87%d0%b5%d0%bd%d0%b8%d0%b5-%d0%b4%d0%be-35-%d0%ba%d0%b2/",
-    #     }
+    "ПОДДЕРЖКА": {
+        "title":"ПОДДЕРЖКА",
+        "link":"#", 
+        "content": {
+            "Каталоги_продукции":{
+                "link":"https://terussia.ru/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B8-%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%86%D0%B8%D0%B8",
+                "content":{},
+            },
+            "Техподдержка_и_обучение_110_и_220_кВ":{
+                "link":"#",
+                "content":{},    
+            },
+            "Обучение":{
+                "link":"https://terussia.ru/%d0%be%d0%b1%d1%83%d1%87%d0%b5%d0%bd%d0%b8%d0%b5/",
+                "content":{},  
+            },
+            "Техподдержка_и_обучение_до_35_кВ":{
+                "link":"https://terussia.ru/%d1%82%d0%b5%d1%85%d0%bf%d0%be%d0%b4%d0%b4%d0%b5%d1%80%d0%b6%d0%ba%d0%b0-%d0%b8-%d0%be%d0%b1%d1%83%d1%87%d0%b5%d0%bd%d0%b8%d0%b5-%d0%b4%d0%be-35-%d0%ba%d0%b2/",
+                "content":{},    
+            },
+        }
 
-    # },
-    # "КОНТАКТЫ": {
-    #     "title":"КОНТАКТЫ",
-    #     "link":"#", 
-    #     "content": {
-    #         "Центральный филиал":"https://terussia.ru/%d1%86%d0%b5%d0%bd%d1%82%d1%80%d0%b0%d0%bb%d1%8c%d0%bd%d1%8b%d0%b9-%d1%84%d0%b8%d0%bb%d0%b8%d0%b0%d0%bb/",
-    #         "Представительства в регионах":"https://terussia.ru/%d0%bf%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b0%d0%b2%d0%b8%d1%82%d0%b5%d0%bb%d1%8c%d1%81%d1%82%d0%b2%d0%b0-%d0%b2-%d1%80%d0%b5%d0%b3%d0%b8%d0%be%d0%bd%d0%b0%d1%85/",
-    #         "Производство Углич":"#",
-    #         "Представительство СНГ-Восток":"https://terussia.ru/%d0%bf%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b0%d0%b2%d0%b8%d1%82%d0%b5%d0%bb%d1%8c%d1%81%d1%82%d0%b2%d0%be-%d1%81%d0%bd%d0%b3-%d0%b2%d0%be%d1%81%d1%82%d0%be%d0%ba/",
-    #         "Представительство СНГ-Запад":"https://terussia.ru/%d0%bf%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b0%d0%b2%d0%b8%d1%82%d0%b5%d0%bb%d1%8c%d1%81%d1%82%d0%b2%d0%be-%d1%81%d0%bd%d0%b3-%d0%b7%d0%b0%d0%bf%d0%b0%d0%b4/",
-    #         "Отдел по работе с клиентами":"https://terussia.ru/%d0%be%d1%82%d0%b4%d0%b5%d0%bb-%d0%bf%d0%be-%d1%80%d0%b0%d0%b1%d0%be%d1%82%d0%b5-%d1%81-%d0%ba%d0%bb%d0%b8%d0%b5%d0%bd%d1%82%d0%b0%d0%bc%d0%b8/",
-    #     }
+    },
+    "КОНТАКТЫ": {
+        "title":"КОНТАКТЫ",
+        "link":"#", 
+        "content": {
+            "Центральный филиал":{
+                "link":"https://terussia.ru/%d1%86%d0%b5%d0%bd%d1%82%d1%80%d0%b0%d0%bb%d1%8c%d0%bd%d1%8b%d0%b9-%d1%84%d0%b8%d0%bb%d0%b8%d0%b0%d0%bb/",
+                "content":{},    
+            },
+            "Представительства в регионах":{
+                "link":"https://terussia.ru/%d0%bf%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b0%d0%b2%d0%b8%d1%82%d0%b5%d0%bb%d1%8c%d1%81%d1%82%d0%b2%d0%b0-%d0%b2-%d1%80%d0%b5%d0%b3%d0%b8%d0%be%d0%bd%d0%b0%d1%85/",
+                "content":{},    
+            },
+            "Производство Углич":{
+                "link":"#",
+                "content":{},    
+            },
+            "Представительство СНГ-Восток":{
+                "link":"https://terussia.ru/%d0%bf%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b0%d0%b2%d0%b8%d1%82%d0%b5%d0%bb%d1%8c%d1%81%d1%82%d0%b2%d0%be-%d1%81%d0%bd%d0%b3-%d0%b2%d0%be%d1%81%d1%82%d0%be%d0%ba/",
+                "content":{},    
+            },
+            "Представительство СНГ-Запад":{
+                "link":"https://terussia.ru/%d0%bf%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b0%d0%b2%d0%b8%d1%82%d0%b5%d0%bb%d1%8c%d1%81%d1%82%d0%b2%d0%be-%d1%81%d0%bd%d0%b3-%d0%b7%d0%b0%d0%bf%d0%b0%d0%b4/",
+                "content": {},
+            },
+            "Отдел по работе с клиентами":{
+                "link":"https://terussia.ru/%d0%be%d1%82%d0%b4%d0%b5%d0%bb-%d0%bf%d0%be-%d1%80%d0%b0%d0%b1%d0%be%d1%82%d0%b5-%d1%81-%d0%ba%d0%bb%d0%b8%d0%b5%d0%bd%d1%82%d0%b0%d0%bc%d0%b8/",
+                "content":{},    
+            },
+        }
 
-    # },
+    },
 } 
